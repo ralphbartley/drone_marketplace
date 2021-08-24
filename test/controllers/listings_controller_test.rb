cb08189d99a2861c8312d6018ebf2f4d13b31635
Felix Piercy <74382928+fel1xp@users.noreply.github.com>
2021-08-24 11:35:44 +0100

    Added controller tests
    Co-authored-by: Ralph <ralphgbartley@gmail.com>

--- test/controllers/listings_controller_test.rb
+++ test/controllers/listings_controller_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class ListingsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get listings_url
+    assert_response :success
+  end
+
+  test "should create new listing" do
+    assert_difference("Listing.count") do
+      post listings_url, params: { listing: { title: "Test Title",
+      price: 100, description: "Test Description"} }
+    end
+    assert_redirected_to listing_path(Listing.last)
+  end
+  
 end
