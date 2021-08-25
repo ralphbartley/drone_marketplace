939bbfc9a11a85745a2f0e46642747f3527fb882
Ralph Bartley <ralphgbartley@gmail.com>
2021-08-25 12:19:14 +0100

    red green listings controller can create and redirect
    Co-authored-by: Felix <felixcpiercy@gmail.com>

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,4 +2,17 @@ class ListingsController < ApplicationController
   def index
     @listings = Listing.all
   end
+
+  def create
+    @listing = Listing.new(post_params)
+    if @listing.save == true
+      redirect_to listing_path(Listing.last)
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:listing).permit(:title, :price, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "listings#index"
-  get "/listings", to: "listings#index"
+
+  resources :listings
 end
