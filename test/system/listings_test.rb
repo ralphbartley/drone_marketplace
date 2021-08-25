f086fe3477475459bd826ab4f4746a616997522f
Ralph Bartley <ralphgbartley@gmail.com>
2021-08-25 15:16:01 +0100

    red green system test displays four listings
    Co-authored-by: Felix <felixcpiercy@gmail.com>

--- test/system/listings_test.rb
+++ test/system/listings_test.rb
@@ -2,8 +2,17 @@ require "application_system_test_case"
 
 class ListingsTest < ApplicationSystemTestCase
   test "visiting the index" do
-   visit listings_url
+    visit listings_url
 
-   assert_selector "h1", text: "Drone Marketplace"
+    assert_selector "h1", text: "Drone Marketplace"
+  end
+
+  test "the index shows the 4 latest articles" do
+    visit listings_url
+
+    assert_selector "h3", text: "Drone 1"
+    assert_selector "h3", text: "Drone 2"
+    assert_selector "h3", text: "Drone 3"
+    assert_selector "h3", text: "Drone 4"
   end
 end
